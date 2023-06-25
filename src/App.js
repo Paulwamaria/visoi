70659de5ceebc6b42e8bb78ff1d4543a5a5bd7a3
Paul Wamaria <paulwamaria@gmail.com>
2023-06-25 20:42:22 +0300

    Create a complete footer with images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Fragment} from "react";
 import "./App.css";
 import Mynavbar from "./components/navbar";
+import Footer from "./components/footer"
 
 function App() {
     return (<Fragment>
         <div className="App">
             <Mynavbar/>
+            <Footer/>
         </div>
     </Fragment>);
 }
