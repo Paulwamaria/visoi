f9ae3432814d0714141f0884520a95d7c13ee521
Paul Wamaria <paulwamaria@gmail.com>
2023-07-09 15:18:35 +0300

    Commented out the non working links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -18,20 +18,15 @@ class Mynavbar extends React.Component {
 							<Navbar.Toggle className="bg-white" aria-controls="basic-navbar-nav"> <i className="fa fa-align-justify text-primary"/> </Navbar.Toggle>
 							<Navbar.Collapse id="basic-navbar-nav">
 								<Nav className="text-white mx-auto">
-									<Nav.Link href="/" className="text-white sofia navLink">About</Nav.Link>
+									<Nav.Link href="/" className="text-white sofia navLink">Home</Nav.Link>
 									<Nav.Link href="/gallery" className="text-white sofia navLink">Gallery</Nav.Link>
-									<Nav.Link href="/testimonials" className="text-white sofia navLink">Testimonials</Nav.Link>
-									<Nav.Link href="/blog" className="text-white sofia navLink">Blog</Nav.Link>
+									{/* <Nav.Link href="/testimonials" className="text-white sofia navLink">Testimonials</Nav.Link>
+									<Nav.Link href="/blog" className="text-white sofia navLink">Blog</Nav.Link> */}
 									<Nav.Link href="/contacts" className="text-white sofia navLink">Contacts</Nav.Link>
 								</Nav>
 								<ul className="navbar-nav text-white mr-auto nav-flex-icons">
 									<li className="nav-item text-white">
-										<a href="/" className="nav-link waves-effect waves-light">
-											<i className="fa fa-twitter text-white navLink"></i>
-										</a>
-									</li>
-									<li className="nav-item text-white">
-										<a href="/" className="nav-link waves-effect waves-light">
+										<a href="https://youtube.com/@josephmburu9101" className="nav-link waves-effect waves-light">
 											<i className="fa fa-youtube-play text-white navLink"></i>
 										</a>
 									</li>
@@ -41,12 +36,12 @@ class Mynavbar extends React.Component {
 										</a>
 									</li>
 									<li className="nav-item text-white">
-										<a href="/" className="nav-link waves-effect waves-light">
-											<i className="fa fa-linkedin text-white navLink"></i>
+										<a href="https://instagram.com/josephmburungethe?igshid=ZDc4ODBmNjlmNQ==" className="nav-link waves-effect waves-light">
+										<i className="fa fa-instagram text-white navLink"></i>
 										</a>
 									</li>
 									<li className="nav-item">
-										<a href="/" className="nav-link waves-effect waves-light">
+										<a href="mailto:Mburujosehp383@gmail.com" className="nav-link waves-effect waves-light">
 											<i className="fa fa-envelope-o text-white navLink"></i>
 										</a>
 									</li>
@@ -61,10 +56,10 @@ class Mynavbar extends React.Component {
 							element={<Home/>}></Route>
 						<Route exact path="/gallery"
 							element={<Gallery/>}></Route>
-						<Route exact path="/testimonials"
+						{/* <Route exact path="/testimonials"
 							element={<Testimonials/>}></Route>
 						<Route exact path="/blog"
-							element={<Blog/>}></Route>
+							element={<Blog/>}></Route> */}
 						<Route exact path="/contacts"
 							element={<Contacts/>}></Route>
 					</Routes>
